87e0200bc8dd52a5c8b185dfbd5b3a1003f9fc5d
chalith <desaman.chalith@gmail.com>
2025-03-21 12:40:28 +0530

    Added cors origins

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,7 @@ const initEnv = require("./environment");
 const multer = require("multer");
 const path = require("path");
 const app = express();
+const cors = require("cors");
 
 // Load SSL certificate and key
 const sslOptions = fs.existsSync(path.resolve(__dirname, "ssl"))
@@ -27,6 +28,22 @@ const port = process.env.NON_SSL_PORT ?? 80;
 const leaseService = new LeaseService();
 
 app.use(express.json());
+
+const allowedOrigins = process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(",") : [];
+console.log("Allowed origins: ", allowedOrigins);
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = "The CORS policy for this site does not allow access from the specified origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Lease Management API");
